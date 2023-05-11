a49065537e62563d6e041649b88de6b2ba66ee51
Rahien <karel.kremer@gmail.com>
2023-05-11 20:04:29 +0200

    bump dependencies

--- frontend/src/BuildOrder.tsx
+++ frontend/src/BuildOrder.tsx
@@ -1,67 +1,84 @@
-import React, {useEffect, useRef, useState} from 'react';
-import BuildOrderStep, {foodIcons, getStepDuration, stepCanProduce, woodIcons} from "./BuildOrderStep";
+import React, { useEffect, useRef, useState } from "react";
+import BuildOrderStep, {
+  foodIcons,
+  getStepDuration,
+  stepCanProduce,
+  woodIcons,
+} from "./BuildOrderStep";
 import BuildOrderTracker from "./BuildOrderTracker";
 import BuildOrderHeader from "./BuildOrderHeader";
 import usePlayingState from "./BuildOrderPlayingStateHook";
-import {IBuildOrder, IBuildOrderStep, IResourceChange, ISortableBuildOrderStep} from "./types";
-import {getBuildOrder} from "./BuildOrderList";
-import { useParams } from 'react-router-dom';
+import {
+  IBuildOrder,
+  IBuildOrderStep,
+  IResourceChange,
+  ISortableBuildOrderStep,
+} from "./types";
+import { getBuildOrder } from "./BuildOrderList";
+import { useParams } from "react-router-dom";
 import SpeedControls from "./SpeedControls";
-import {v4} from "uuid";
-import {useSetting} from "./hooks";
+import { v4 } from "uuid";
+import { useSetting } from "./hooks";
 import Footer from "./Footer";
 
-export const stepKinds: {[id:string]:string} = {
- "create": "Create Villager",
- "build": "Build",
- "loom": "Get Loom",
- "move": "Move",
- "research": "Research",
- "wheelbarrow": "Wheelbarrow",
- "note": "Note",
- "age2": "Feudal Age",
- "age3": "Castle Age",
- "age4": "Imperial Age"
+export const stepKinds: { [id: string]: string } = {
+  create: "Create Villager",
+  build: "Build",
+  loom: "Get Loom",
+  move: "Move",
+  research: "Research",
+  wheelbarrow: "Wheelbarrow",
+  note: "Note",
+  age2: "Feudal Age",
+  age3: "Castle Age",
+  age4: "Imperial Age",
+};
 
-}
-
-const addResourcesFromStep = (buildOrder:IBuildOrder, step:IBuildOrderStep, percentageComplete: number = 1) => {
-  if(step.kind === "create" || (step.kind === "build" && ['villager', 'villagerf'].indexOf(step.from || "") >= 0)){
+const addResourcesFromStep = (
+  buildOrder: IBuildOrder,
+  step: IBuildOrderStep,
+  percentageComplete: number = 1
+) => {
+  if (
+    step.kind === "create" ||
+    (step.kind === "build" &&
+      ["villager", "villagerf"].indexOf(step.from || "") >= 0)
+  ) {
     const number = Math.floor((step.number || 1) * percentageComplete);
     buildOrder.currentVillagers = (buildOrder.currentVillagers || 0) + number;
   }
   const checkResources = stepCanProduce(step);
-  if(checkResources && step.target){
+  if (checkResources && step.target) {
     const number = Math.floor((step.number || 1) * percentageComplete);
-    if(foodIcons.indexOf(step.target) >= 0){
+    if (foodIcons.indexOf(step.target) >= 0) {
       buildOrder.currentFood = (buildOrder.currentFood || 0) + number;
     }
-    if(woodIcons.indexOf(step.target) >= 0){
+    if (woodIcons.indexOf(step.target) >= 0) {
       buildOrder.currentWood = (buildOrder.currentWood || 0) + number;
     }
-    if(step.target === "gold"){
+    if (step.target === "gold") {
       buildOrder.currentGold = (buildOrder.currentGold || 0) + number;
     }
-    if(step.target === "stone"){
+    if (step.target === "stone") {
       buildOrder.currentStone = (buildOrder.currentStone || 0) + number;
     }
   }
-  if(checkResources && step.from){
+  if (checkResources && step.from) {
     const number = Math.floor((step.number || 1) * percentageComplete);
-    if(foodIcons.indexOf(step.from) >= 0){
+    if (foodIcons.indexOf(step.from) >= 0) {
       buildOrder.currentFood = (buildOrder.currentFood || 0) - number;
     }
-    if(woodIcons.indexOf(step.from) >= 0){
+    if (woodIcons.indexOf(step.from) >= 0) {
       buildOrder.currentWood = (buildOrder.currentWood || 0) - number;
     }
-    if(step.from === "gold"){
+    if (step.from === "gold") {
       buildOrder.currentGold = (buildOrder.currentGold || 0) - number;
     }
-    if(step.from === "stone"){
+    if (step.from === "stone") {
       buildOrder.currentStone = (buildOrder.currentStone || 0) - number;
     }
   }
-  if(step.subSteps){
+  if (step.subSteps) {
     step.subSteps.forEach((step) => {
       addResourcesFromStep(buildOrder, step, percentageComplete);
     });
@@ -73,23 +90,26 @@ export const computeStartingVillagers = (buildOrder: IBuildOrder) => {
   const stepsToCheck = ([] as IBuildOrderStep[]).concat(buildOrder.steps);
   let seenCreate = false;
   stepsToCheck.forEach((step) => {
-    if(seenCreate){
+    if (seenCreate) {
       return;
     }
-    if(step.kind === "create") {
+    if (step.kind === "create") {
       seenCreate = true;
       return;
     }
-    count += (step.number || 0);
+    count += step.number || 0;
   });
 
-  if(count === 0){
+  if (count === 0) {
     return 4;
   }
   return count;
-}
+};
 
-export const addResourcesUpToCurrentStep = (buildOrder:IBuildOrder, gameTime:number) => {
+export const addResourcesUpToCurrentStep = (
+  buildOrder: IBuildOrder,
+  gameTime: number
+) => {
   let currentStepSeen = false;
   buildOrder.currentVillagers = computeStartingVillagers(buildOrder);
   buildOrder.currentStone = 0;
@@ -97,254 +117,299 @@ export const addResourcesUpToCurrentStep = (buildOrder:IBuildOrder, gameTime:num
   buildOrder.currentWood = 0;
   buildOrder.currentFood = 0;
   buildOrder.steps.forEach((step) => {
-    if(currentStepSeen && gameTime > 0){
+    if (currentStepSeen && gameTime > 0) {
       return;
     }
-    if(step === buildOrder.currentStep && gameTime > 0){
+    if (step === buildOrder.currentStep && gameTime > 0) {
       currentStepSeen = true;
     }
-    addResourcesFromStep(buildOrder, step,
-      currentStepSeen?buildOrder.currentStepPercentage:1);
+    addResourcesFromStep(
+      buildOrder,
+      step,
+      currentStepSeen ? buildOrder.currentStepPercentage : 1
+    );
   });
 };
 
-export const shuffleVillagerGenders: (steps: IBuildOrderStep[]) => void = (steps) => {
+export const shuffleVillagerGenders: (steps: IBuildOrderStep[]) => void = (
+  steps
+) => {
   steps.forEach((step) => {
     step.femaleVillager = Math.random() < 0.5;
   });
-}
+};
 
-const getCurrentStepIndex = (buildOrder:IBuildOrder, currentTime:number) => {
+const getCurrentStepIndex = (buildOrder: IBuildOrder, currentTime: number) => {
   let currentIndex = -1;
   buildOrder.steps.forEach((step, index) => {
-    if((step.endTime || 0) <= currentTime){
+    if ((step.endTime || 0) <= currentTime) {
       currentIndex = index;
     }
   });
   return currentIndex + 1;
 };
 
-export const getNextRelevantMoment = (buildOrder:IBuildOrder, currentTime: number) => {
+export const getNextRelevantMoment = (
+  buildOrder: IBuildOrder,
+  currentTime: number
+) => {
   let currentIndex = getCurrentStepIndex(buildOrder, currentTime);
-  if(currentIndex < 0){
+  if (currentIndex < 0) {
     currentIndex = 0;
   }
   let currentStep = buildOrder.steps[currentIndex];
-  if(!currentStep){
-    currentStep = buildOrder.steps[buildOrder.steps.length -1];
+  if (!currentStep) {
+    currentStep = buildOrder.steps[buildOrder.steps.length - 1];
   }
-  return ( currentStep.endTime || 0 )+ 0.01;
-}
+  return (currentStep.endTime || 0) + 0.01;
+};
 
-export const getPreviousRelevantMoment = (buildOrder:IBuildOrder, currentTime: number) => {
+export const getPreviousRelevantMoment = (
+  buildOrder: IBuildOrder,
+  currentTime: number
+) => {
   let currentIndex = getCurrentStepIndex(buildOrder, currentTime);
-  if(currentIndex <= 0){
+  if (currentIndex <= 0) {
     return 0;
   }
   let currentStep = buildOrder.steps[currentIndex - 1];
   let duration = getStepDuration(currentStep);
-  while(duration === 0 && currentIndex > 0){
+  while (duration === 0 && currentIndex > 0) {
     currentIndex -= 1;
     currentStep = buildOrder.steps[currentIndex - 1];
     duration = (currentStep && getStepDuration(currentStep)) || 0;
   }
-  if(!currentStep){
+  if (!currentStep) {
     return 0;
   }
-  return ( currentStep.endTime || 0 )- duration;
-}
+  return (currentStep.endTime || 0) - duration;
+};
 
-export const computeEndTimes = (buildOrderSteps:IBuildOrderStep[]) => {
+export const computeEndTimes = (buildOrderSteps: IBuildOrderStep[]) => {
   let time = 0;
   let age = "age1";
   buildOrderSteps.forEach((step) => {
     step.age = age;
     time += getStepDuration(step);
     step.endTime = time;
-    if(step.kind === "age2" ||
-      (step.kind === "research" && (step.techs || []).indexOf("age2") >= 0)){
+    if (
+      step.kind === "age2" ||
+      (step.kind === "research" && (step.techs || []).indexOf("age2") >= 0)
+    ) {
       age = "age2";
     }
-    if(step.kind === "age3" ||
-      (step.kind === "research" && (step.techs || []).indexOf("age3") >= 0)){
+    if (
+      step.kind === "age3" ||
+      (step.kind === "research" && (step.techs || []).indexOf("age3") >= 0)
+    ) {
       age = "age3";
     }
-    if(step.kind === "age4" ||
-      (step.kind === "research" && (step.techs || []).indexOf("age4") >= 0)){
+    if (
+      step.kind === "age4" ||
+      (step.kind === "research" && (step.techs || []).indexOf("age4") >= 0)
+    ) {
       age = "age4";
     }
   });
-}
+};
 
-export const computeResourceChanges = (buildOrderSteps:IBuildOrderStep[]) => {
-  const resources:{[id:string]: number} = {food: 0, wood: 0, gold: 0, stone: 0};
+export const computeResourceChanges = (buildOrderSteps: IBuildOrderStep[]) => {
+  const resources: { [id: string]: number } = {
+    food: 0,
+    wood: 0,
+    gold: 0,
+    stone: 0,
+  };
   const resourcesToTrack = Object.keys(resources);
   buildOrderSteps.forEach((step) => {
     const number = step.number || 1;
     step.resourceChanges = [] as IResourceChange[];
-    if(!stepCanProduce(step)){
+    if (!stepCanProduce(step)) {
       return;
     }
     let from = step.from;
-    if(from && foodIcons.indexOf(from) >= 0){
+    if (from && foodIcons.indexOf(from) >= 0) {
       from = "food";
     }
-    if(from && woodIcons.indexOf(from) >= 0){
+    if (from && woodIcons.indexOf(from) >= 0) {
       from = "wood";
     }
     let to = step.target;
-    if(to && foodIcons.indexOf(to) >= 0){
+    if (to && foodIcons.indexOf(to) >= 0) {
       to = "food";
     }
-    if(to && woodIcons.indexOf(to) >= 0){
+    if (to && woodIcons.indexOf(to) >= 0) {
       to = "wood";
     }
-    if(from === to){
+    if (from === to) {
       return;
     }
     if (from && resourcesToTrack.indexOf(from) >= 0) {
       let newResource = (resources[from] || 0) - number;
-      resources[from] = newResource
+      resources[from] = newResource;
       step.resourceChanges.push({
         resource: from,
         target: newResource,
-        direction: "down"
+        direction: "down",
       });
-
     }
-    if(to && resourcesToTrack.indexOf(to) >= 0){
+    if (to && resourcesToTrack.indexOf(to) >= 0) {
       let newResourceTo = (resources[to] || 0) + number;
-      resources[to] = newResourceTo
+      resources[to] = newResourceTo;
       step.resourceChanges.push({
         resource: to,
         target: newResourceTo,
-        direction: "up"
+        direction: "up",
       });
     }
   });
-}
+};
 
-export const mergeSubsteps: (steps: IBuildOrderStep[]) => IBuildOrderStep[] = (buildOrderSteps) => {
-  const newSteps:IBuildOrderStep[] = [];
-  let previousStep:(IBuildOrderStep|null) = null;
+export const mergeSubsteps: (steps: IBuildOrderStep[]) => IBuildOrderStep[] = (
+  buildOrderSteps
+) => {
+  const newSteps: IBuildOrderStep[] = [];
+  let previousStep: IBuildOrderStep | null = null;
   buildOrderSteps.forEach((step) => {
-    if(step.duringPrevious && previousStep){
+    if (step.duringPrevious && previousStep) {
       previousStep.subSteps = previousStep.subSteps || [];
       previousStep.subSteps.push(step);
-    }else{
+    } else {
       newSteps.push(step);
       previousStep = step;
     }
   });
   return newSteps;
-}
+};
 
-export const unfoldSubsteps: (steps: IBuildOrderStep[]) => ISortableBuildOrderStep[] = (buildOrderSteps) => {
-  const newSteps:IBuildOrderStep[] = [];
+export const unfoldSubsteps: (
+  steps: IBuildOrderStep[]
+) => ISortableBuildOrderStep[] = (buildOrderSteps) => {
+  const newSteps: IBuildOrderStep[] = [];
   buildOrderSteps.forEach((step) => {
     newSteps.push(step);
-    if(step.subSteps) {
+    if (step.subSteps) {
       step.subSteps.forEach((substep) => {
         newSteps.push(substep);
       });
     }
     delete step.subSteps;
   });
-  const sortableSteps:ISortableBuildOrderStep[] = newSteps.map((step:any) => {
+  const sortableSteps: ISortableBuildOrderStep[] = newSteps.map((step: any) => {
     step.id = v4();
     return step as ISortableBuildOrderStep;
   });
   return sortableSteps;
-}
+};
 
-const requestWakeLock = (wakeLockRef:any) => {
-  try{
-    if ('wakeLock' in window.navigator) {
+const requestWakeLock = (wakeLockRef: any) => {
+  try {
+    if ("wakeLock" in window.navigator) {
       // @ts-ignore
-      window.navigator.wakeLock.request('screen').then((wakeLock:any) => {
-        wakeLockRef.current = wakeLock;
-      }).catch(() => {
-        // maybe next time
-      });
+      window.navigator.wakeLock
+        .request("screen")
+        .then((wakeLock: any) => {
+          wakeLockRef.current = wakeLock;
+        })
+        .catch(() => {
+          // maybe next time
+        });
     }
-  } catch (err) {
-  }
-}
+  } catch (err) {}
+};
 
-const releaseWakeLock = (wakeLockRef:any) => {
+const releaseWakeLock = (wakeLockRef: any) => {
   wakeLockRef.current && wakeLockRef.current.release();
-}
+};
 
-const computeEndTime:(buildOrder:IBuildOrder) => number = (buildOrder) => {
-  try{
+const computeEndTime: (buildOrder: IBuildOrder) => number = (buildOrder) => {
+  try {
     return buildOrder.steps[buildOrder.steps.length - 1].endTime || 0;
-  }catch (e){
+  } catch (e) {
     return 0;
   }
-}
+};
 
 function BuildOrder() {
-  const [startTime, playing, timeAlreadyPlayed, togglePlaying, updateGameTime] = usePlayingState();
+  const [startTime, playing, timeAlreadyPlayed, togglePlaying, updateGameTime] =
+    usePlayingState();
   const [gameTime, setGameTime] = useState(0);
-  const [buildOrder, setBuildOrder] = useState<IBuildOrder|null>(null);
+  const [buildOrder, setBuildOrder] = useState<IBuildOrder | null>(null);
   const wakeLockRef = useRef<any>();
   const showPerVillagerMarks = useSetting<boolean>("markingPerVil", true);
-  const {id} = useParams();
+  const { id } = useParams<{ id: string }>();
   useEffect(() => {
     const build = getBuildOrder(id);
     addResourcesUpToCurrentStep(build, 0);
     build.steps = mergeSubsteps(build.steps);
-    setBuildOrder(build)
+    setBuildOrder(build);
   }, [id]);
   useEffect(() => {
     requestWakeLock(wakeLockRef);
     return () => {
       releaseWakeLock(wakeLockRef);
-    }
+    };
   }, [playing, wakeLockRef]);
 
-  if(!buildOrder){
+  if (!buildOrder) {
     return <div>Loading</div>;
   }
 
-  const changeGameTime = (time:number, pause= true, withUpdate = true) => {
-    const endTime = computeEndTime(buildOrder)
+  const changeGameTime = (time: number, pause = true, withUpdate = true) => {
+    const endTime = computeEndTime(buildOrder);
     time = Math.min(endTime, Math.max(time, 0));
     setGameTime(time);
-    if(time === endTime){
+    if (time === endTime) {
       togglePlaying(gameTime, false);
     }
-    if(withUpdate){
+    if (withUpdate) {
       updateGameTime(time, pause);
     }
-  }
+  };
 
-  const onNewBuildOrderState = (newBuildOrder:IBuildOrder) => {
+  const onNewBuildOrderState = (newBuildOrder: IBuildOrder) => {
     addResourcesUpToCurrentStep(newBuildOrder, gameTime);
     setBuildOrder(newBuildOrder);
   };
   const steps = buildOrder.steps.map((step, index) => {
-    return <div className="buildorder-step-wrap" key={index}>
-      <BuildOrderStep step={step} setGameTime={changeGameTime}/>
-    </div>;
+    return (
+      <div className="buildorder-step-wrap" key={index}>
+        <BuildOrderStep step={step} setGameTime={changeGameTime} />
+      </div>
+    );
   });
   const showTracker = timeAlreadyPlayed || playing;
   return (
-    <div className={`buildOrder ${showPerVillagerMarks?"per-villager-marks":""}`}>
-      <BuildOrderHeader buildOrder={buildOrder} gameTime={gameTime} setGameTime={changeGameTime}/>
+    <div
+      className={`buildOrder ${
+        showPerVillagerMarks ? "per-villager-marks" : ""
+      }`}
+    >
+      <BuildOrderHeader
+        buildOrder={buildOrder}
+        gameTime={gameTime}
+        setGameTime={changeGameTime}
+      />
       {steps}
-      {showTracker?<BuildOrderTracker buildOrder={buildOrder} startTime={startTime}
-                         elapsedTime={timeAlreadyPlayed}
-                         playing={playing}
-                         gameTimeChange={(time) => changeGameTime(time, false, false)}
-                         onNewBuildOrderState={onNewBuildOrderState}/>:null}
-      <SpeedControls gameTime={gameTime}
-                     buildOrder={buildOrder}
-                     playing={playing}
-                     togglePlaying={() => togglePlaying(gameTime)}
-                     setGameTime={changeGameTime}/>
+      {showTracker ? (
+        <BuildOrderTracker
+          buildOrder={buildOrder}
+          startTime={startTime}
+          elapsedTime={timeAlreadyPlayed}
+          playing={playing}
+          gameTimeChange={(time) => changeGameTime(time, false, false)}
+          onNewBuildOrderState={onNewBuildOrderState}
+        />
+      ) : null}
+      <SpeedControls
+        gameTime={gameTime}
+        buildOrder={buildOrder}
+        playing={playing}
+        togglePlaying={() => togglePlaying(gameTime)}
+        setGameTime={changeGameTime}
+      />
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- frontend/src/EditBuildOrder.tsx
+++ frontend/src/EditBuildOrder.tsx
@@ -1,31 +1,38 @@
-import React, {useEffect, useState} from "react";
-import {faCheck, faPlus, faUpload} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { faCheck, faPlus, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams, useHistory } from "react-router-dom";
 import UploadBuildOrder from "./UploadBuildOrder";
-import {IBuildOrder, IBuildOrderStep, ISortableBuildOrder, ISortableBuildOrderStep} from "./types";
-import {getBuildOrder, setBuildOrder} from "./BuildOrderList";
+import {
+  IBuildOrder,
+  IBuildOrderStep,
+  ISortableBuildOrder,
+  ISortableBuildOrderStep,
+} from "./types";
+import { getBuildOrder, setBuildOrder } from "./BuildOrderList";
 import BuildOrderIconSelect from "./BuildOrderIconSelect";
 import {
   addResourcesUpToCurrentStep,
   computeEndTimes,
   computeResourceChanges,
-  shuffleVillagerGenders
+  shuffleVillagerGenders,
 } from "./BuildOrder";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { ReactSortable } from "react-sortablejs";
 import BuildOrderStepEdit from "./BuildOrderStepEdit";
 
-const makeStepsSortable:(steps:IBuildOrderStep[]) => ISortableBuildOrderStep[] = (steps) => {
+const makeStepsSortable: (
+  steps: IBuildOrderStep[]
+) => ISortableBuildOrderStep[] = (steps) => {
   return steps.map((step) => {
-    return Object.assign({id: uuidv4()}, step);
+    return Object.assign({ id: uuidv4() }, step);
   });
-}
+};
 
 function EditBuildOrder() {
-  const {id} = useParams();
+  const { id } = useParams<{ id: string }>();
   const history = useHistory();
-  const [build, setBuild] = useState<ISortableBuildOrder|null>(null);
+  const [build, setBuild] = useState<ISortableBuildOrder | null>(null);
   useEffect(() => {
     const buildOrder = getBuildOrder(id);
     const sortableBuildOrder = Object.assign({}, buildOrder);
@@ -33,24 +40,27 @@ function EditBuildOrder() {
     setBuild(sortableBuildOrder as unknown as ISortableBuildOrder);
   }, [id]);
   const [loadingFromFile, setLoadingFromFile] = useState(false);
-  if(!build){
-    return <div>Loading...</div>
+  if (!build) {
+    return <div>Loading...</div>;
   }
-  const updateBuild = (build:ISortableBuildOrder, shuffle:boolean = false) => {
+  const updateBuild = (
+    build: ISortableBuildOrder,
+    shuffle: boolean = false
+  ) => {
     addResourcesUpToCurrentStep(build, 0);
     computeEndTimes(build.steps);
     computeResourceChanges(build.steps);
-    if(shuffle){
+    if (shuffle) {
       shuffleVillagerGenders(build.steps);
     }
     setBuild(build);
-  }
+  };
   const loadFromFile = () => {
     setLoadingFromFile(true);
   };
-  const onUpload = (newBuild:IBuildOrder|null) => {
+  const onUpload = (newBuild: IBuildOrder | null) => {
     setLoadingFromFile(false);
-    if(newBuild){
+    if (newBuild) {
       newBuild.name = build.name;
       setBuildOrder(id, newBuild);
       history.go(0);
@@ -59,78 +69,97 @@ function EditBuildOrder() {
 
   const addStep = () => {
     const newBuild = Object.assign({}, build);
-    newBuild.steps = ([] as ISortableBuildOrderStep[]).concat(...newBuild.steps);
+    newBuild.steps = ([] as ISortableBuildOrderStep[]).concat(
+      ...newBuild.steps
+    );
     newBuild.steps.push({
       kind: "create",
       target: "sheep",
-      id: uuidv4()
+      id: uuidv4(),
     });
     updateBuild(newBuild, true);
   };
 
-  const getStepUpdateHandler = (index:number) => {
-      return (newStep:ISortableBuildOrderStep) => {
-        const newBuild = Object.assign({}, build);
-        newBuild.steps = ([] as ISortableBuildOrderStep[]).concat(...newBuild.steps);
-        newBuild.steps.splice(index, 1, newStep);
-        updateBuild(newBuild, true);
-      };
-  }
+  const getStepUpdateHandler = (index: number) => {
+    return (newStep: ISortableBuildOrderStep) => {
+      const newBuild = Object.assign({}, build);
+      newBuild.steps = ([] as ISortableBuildOrderStep[]).concat(
+        ...newBuild.steps
+      );
+      newBuild.steps.splice(index, 1, newStep);
+      updateBuild(newBuild, true);
+    };
+  };
 
-  const removeStep = (index:number) => {
+  const removeStep = (index: number) => {
     const newBuild = Object.assign({}, build);
-    newBuild.steps = ([] as ISortableBuildOrderStep[]).concat(...newBuild.steps);
+    newBuild.steps = ([] as ISortableBuildOrderStep[]).concat(
+      ...newBuild.steps
+    );
     newBuild.steps.splice(index, 1);
     updateBuild(newBuild, true);
-  }
+  };
 
-  const reorderSteps = (steps:ISortableBuildOrderStep[]) => {
+  const reorderSteps = (steps: ISortableBuildOrderStep[]) => {
     const newBuild = Object.assign({}, build);
     newBuild.steps = steps;
     updateBuild(newBuild);
   };
 
-  const steps = <ReactSortable delay={1000} list={build.steps} setList={reorderSteps}>
-    {build.steps.map((step, index) => {
-      return <BuildOrderStepEdit key={step.id} step={step}
-                                 onEdit={getStepUpdateHandler(index)}
-                                 onRemove={() => removeStep(index)}/>
-    })}
-  </ReactSortable>
+  const steps = (
+    <ReactSortable delay={1000} list={build.steps} setList={reorderSteps}>
+      {build.steps.map((step, index) => {
+        return (
+          <BuildOrderStepEdit
+            key={step.id}
+            step={step}
+            onEdit={getStepUpdateHandler(index)}
+            onRemove={() => removeStep(index)}
+          />
+        );
+      })}
+    </ReactSortable>
+  );
 
   return (
     <>
       <div className="edit-buildOrder">
         <div className="edit-header">
-          <BuildOrderIconSelect icon={build.icon} onIconSelect={(icon) => {
-            const newBuild = Object.assign({}, build);
-            newBuild.icon = icon;
-            updateBuild(newBuild);
-          }}/>
-          <input value={build.name}
-                 onChange={(e) => {
-                   const newBuild = Object.assign({}, build);
-                   newBuild.name = e.target.value
-                   updateBuild(newBuild);
-                 }}/>
-          <FontAwesomeIcon icon={faCheck} onClick={() => {
-            setBuildOrder(build.id, build);
-            history.push('/');
-          }}/>
+          <BuildOrderIconSelect
+            icon={build.icon}
+            onIconSelect={(icon) => {
+              const newBuild = Object.assign({}, build);
+              newBuild.icon = icon;
+              updateBuild(newBuild);
+            }}
+          />
+          <input
+            value={build.name}
+            onChange={(e) => {
+              const newBuild = Object.assign({}, build);
+              newBuild.name = e.target.value;
+              updateBuild(newBuild);
+            }}
+          />
+          <FontAwesomeIcon
+            icon={faCheck}
+            onClick={() => {
+              setBuildOrder(build.id, build);
+              history.push("/");
+            }}
+          />
         </div>
-        <div className="steps">
-          {steps}
-        </div>
+        <div className="steps">{steps}</div>
         <button className="upload-build" onClick={loadFromFile}>
-          <FontAwesomeIcon icon={faUpload}/>
+          <FontAwesomeIcon icon={faUpload} />
           <label>From File</label>
         </button>
         <button className="add-step" onClick={addStep}>
-          <FontAwesomeIcon icon={faPlus}/>
+          <FontAwesomeIcon icon={faPlus} />
           <label>Add Step</label>
         </button>
       </div>
-      {loadingFromFile?<UploadBuildOrder onUpload={onUpload}/>:null}
+      {loadingFromFile ? <UploadBuildOrder onUpload={onUpload} /> : null}
     </>
   );
 }
